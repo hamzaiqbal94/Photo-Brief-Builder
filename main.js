44cc4aaab9d101dd16e09b2e79aeb5d30d6cb6ee
hamzaiqbal94 <112578805+hamzaiqbal94@users.noreply.github.com>
2023-06-09 23:05:59 -0400

    CSS, JS

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
-
 function generatePDF() {
+  const productName = document.getElementById('product-name').value;
+  const projectName = document.getElementById('project-name').value;
+  const intendedUsage = document.getElementById('intended-usage').value;
+  const format = document.getElementById('format').value;
+  const lens = document.getElementById('lens').value;
+  const orientation = document.getElementById('orientation').value;
+  const lighting = document.getElementById('lighting').value;
+  const cameraAngles = document.getElementById('camera-angles').value;
+  const cameraPerspective = document.getElementById('camera-perspective').value;
+  const lightingAddons = document.getElementById('lighting-addons').value;
 
-    const productName = document.getElementById('product-name').value;
-    const projectName = document.getElementById('project-name').value;
-    const intendedUsage = document.getElementById('intended-usage').value;
-    const format = document.getElementById('format').value;
-    const lens = document.getElementById('lens').value;
-    const orientation = document.getElementById('orientation').value;
-    const lighting = document.getElementById('lighting').value;
-    const cameraAngles = document.getElementById('camera-angles').value;
-    const cameraPerspective = document.getElementById('camera-perspective').value;
-    const lightingAddons = document.getElementById('lighting-addons').value;
-  
-    const documentDefinition = {
-        pageSize: {
-            width: 500,
-            height: 'auto'
-          },
+  const documentDefinition = {
+    pageSize: {
+      width: 500,
+      height: 'auto'
+    },
+    content: [
+      {
+        text: 'PRODUCT PHOTOGRAPHY - CREATIVE BRIEF',
+        style: 'header'
+      },
+      {
+        text: 'Please see specifications for product photography below:',
+        style: 'subheader',
+        margin: [0, 10, 0, 30]
+      },
+      { text: 'Product Name: ' + productName },
+      { text: 'Project Name: ' + projectName },
+      { text: 'Intended Usage: ' + intendedUsage },
+      { text: 'Format of Photos: ' + format },
+      { text: 'Camera Lens: ' + lens },
+      { text: 'Orientation: ' + orientation },
+      { text: 'Lighting: ' + lighting },
+      { text: 'Camera Angles: ' + cameraAngles },
+      { text: 'Camera Perspective: ' + cameraPerspective },
+      { text: 'Lighting Add-ons: ' + lightingAddons },
+      {
+        text: 'Built using Creative Brief Generator - An online tool to streamline product photography sessions which breaks down art direction into actionables that are exported as printable PDFs.',
+        style: 'subheader2',
+        margin: [0, 10, 0, 30]
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10]
+      },
+      subheader: {
+        fontSize: 12,
+        margin: [0, 0, 0, 10]
+      },
+      subheader2: {
+        fontSize: 7,
+        margin: [0, 0, 0, 10]
+      }
+    }
+  };
 
-        
-            
+  pdfMake.createPdf(documentDefinition).open();
+}
 
-
-      content: [
-
-        {
-            text: 'Product Photography Brief',
-            style: 'header'
-          },
-          {
-            text: 'Please see specifications for product photography below.',
-            style: 'subheader',
-            margin: [0, 10, 0, 30]
-          },
-
-          
-
-
-        { text: 'Product Name: ' + productName },
-        { text: 'Project Name: ' + projectName },
-        { text: 'Intended Usage: ' + intendedUsage },
-        { text: 'Format of Photos: ' + format },
-        { text: 'Camera Lens: ' + lens },
-        { text: 'Orientation: ' + orientation },
-        { text: 'Lighting: ' + lighting },
-        { text: 'Camera Angles: ' + cameraAngles },
-        { text: 'Camera Perspective: ' + cameraPerspective },
-        { text: 'Lighting Add-ons: ' + lightingAddons },
-
-        {
-            text: 'Built using Product Photography Brief Builder - An online tool to streamline product photography sessions which breaks down art direction into actionables that are exported as printable PDFs. Final project submission for T&I Spring 2023 by Hamza Iqbal',
-            style: 'subheader',
-            margin: [0, 10, 0, 30]
-          },
-
-      ]
-    };
-  
-    pdfMake.createPdf(documentDefinition).open();
-  }
-  
-  const generatePDFButton = document.getElementById('generate-pdf');
-  generatePDFButton.addEventListener('click', generatePDF);
-  
+const generatePDFButton = document.getElementById('generate-pdf');
+generatePDFButton.addEventListener('click', generatePDF);
